68804cec140ee6f028d3b5cb4a495bcf1e0457ea
David Wickes <dave.wickes@gmail.com>
2017-02-13 22:10:33 +0000

    averages!

--- ch8/fundamental_collections/summary_exercises/averages/src/main.rs
+++ ch8/fundamental_collections/summary_exercises/averages/src/main.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
 
-fn main() {
-    print!("Hello, World");
-    mode(&vec![1, 2, 2]);
-}
+fn main() {}
 
 fn averages(xs: &Vec<i32>) -> (f32, i32, i32) {
     let mean = mean(xs);
-    let median = 0;
-    let mode = 0;
+    let median = median(xs);
+    let mode = mode(xs);
     return (mean, median, mode);
 }
 
@@ -25,14 +22,21 @@ fn mode(xs: &Vec<i32>) -> i32 {
         let v = m.entry(x).or_insert(0);
         *v += 1;
     }
-    println!("{:?}", m);
-    let (v, _) = m.iter().max_by_key(|&(_, count)| count).unwrap();
-    return v;
+    let (v, _) = m.iter()
+        .max_by_key(|&(_, count)| count)
+        .unwrap();
+    return v.to_owned().to_owned();
+}
+
+fn median(xs: &Vec<i32>) -> i32 {
+    let mut ordered = xs.to_owned();
+    ordered.sort();
+    return ordered[ordered.len() / 2];
 }
 
 #[test]
 fn test_averages() {
-    let (mean, median, mode) = averages(&vec![1, 2, 2, 4, 6]);
+    let (mean, median, mode) = averages(&vec![2, 2, 1, 4, 6]);
     assert_eq!(mean, 3.0);
     assert_eq!(median, 2);
     assert_eq!(mode, 2);
@@ -60,4 +64,16 @@ fn mode_of_one_value() {
 fn mode_of_three_values() {
     let v = vec![5, 1, 1];
     assert_eq!(1, mode(&v));
+}
+
+#[test]
+fn median_of_one_value() {
+    let v = vec![3];
+    assert_eq!(3, median(&v));
+}
+
+#[test]
+fn median_of_three_values() {
+    let v = vec![5, 3, 4, 2, 1];
+    assert_eq!(3, median(&v));
 }
