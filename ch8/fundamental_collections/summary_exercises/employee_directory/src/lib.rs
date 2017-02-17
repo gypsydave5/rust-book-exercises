9e60d7e3da7821fb71d1feb0da5eb2af0d10e6fd
David Wickes <dave.wickes@gmail.com>
2017-02-17 10:22:05 +0000

    refactor to use a struct

--- ch8/fundamental_collections/summary_exercises/employee_directory/src/lib.rs
+++ ch8/fundamental_collections/summary_exercises/employee_directory/src/lib.rs
@@ -1,5 +1,25 @@
-pub fn insert(s: &str) {}
+use std::collections::HashMap;
 
-pub fn retrieve(dept: &str) -> String {
-    return String::from("Dept: Engineering\n\tSally\n");
+pub struct Database {
+    db: HashMap<String, Vec<String>>,
 }
+
+pub fn new() -> Database {
+    Database { db: HashMap::new() }
+}
+
+
+impl Database {
+    pub fn insert(&mut self, s: &str) {}
+
+    pub fn retrieve(&self, dept: &str) -> String {
+        return String::from("Dept: Engineering\n\tSally\n");
+    }
+}
+
+// #[test]
+// fn can_parse_a_name_from_the_insert_string() {
+//     let insert_string = "Add Sally to Engineering";
+//     let name = name_from(insert_string);
+//     assert_eq!("Sally", name)
+// }
--- ch8/fundamental_collections/summary_exercises/employee_directory/tests/integration_test.rs
+++ ch8/fundamental_collections/summary_exercises/employee_directory/tests/integration_test.rs
@@ -3,10 +3,33 @@ extern crate employee_directory;
 #[test]
 fn insert_employee_retrieve_department() {
     let insert_string = "Add Sally to Engineering";
+    let mut db = employee_directory::new();
 
-    employee_directory::insert(insert_string);
+    db.insert(insert_string);
+
 
     let expected_list = String::from("Dept: Engineering\n\tSally\n");
-    let employee_list = employee_directory::retrieve("Engineering");
+    let employee_list = db.retrieve("Engineering");
+    assert_eq!(expected_list, employee_list);
+}
+
+#[test]
+fn private_fields_are_private() {
+    // won't compile
+    // db.db.insert("hi".to_string(), vec!["hi".to_string()])
+}
+
+#[test]
+fn insert_two_employees_retrieve_department() {
+    let insert_sally = "Add Sally to Engineering";
+    let insert_roger = "Add Roger to Engineering";
+
+    let mut db = employee_directory::new();
+
+    db.insert(insert_sally);
+    db.insert(insert_roger);
+
+    let expected_list = String::from("Dept: Engineering\n\tSally\n\tRoger\n");
+    let employee_list = db.retrieve("Engineering");
     assert_eq!(expected_list, employee_list);
 }
