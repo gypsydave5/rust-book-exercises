98b9ec52d3eae5400058a7139fc41fe1e2319bfb
David Wickes <dave.wickes@gmail.com>
2017-02-23 17:43:34 +0000

    propagation of errors

--- ch9/panic/src/main.rs
+++ ch9/panic/src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+use std::io::Read;
 use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    expect_is_like_unwrap_but_with_a_custom_error();
+    even_shorter().unwrap();
 }
 
 fn this_kinda_thing_panics() {
@@ -45,4 +47,41 @@ fn unwrap_is_a_helper_on_results_that_panics_when_err() {
 fn expect_is_like_unwrap_but_with_a_custom_error() {
     let filename = "hello.txt";
     let f = File::open(filename).expect(&format!("Failed to open {}", filename));
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // here we propagate the result (and the error);
+
+    let filename = "hello.txt";
+    let f = File::open(filename);
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e)
+    }
+}
+
+fn read_username_from_file_with_question_mark() -> Result<String, io::Error> {
+    // this is exactly the same as the above  - `?` acts like a match with an early return of
+    // `Err<T>` values. Nice.
+    let filename = "hello.txt";
+    let mut f = File::open(filename)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn even_shorter() -> Result<String, io::Error> {
+    let filename = "hello.txt";
+    let mut s = String::new();
+    File::open(filename)?.read_to_string(&mut s)?;
+    // revenge of the Elvis operator :D
+    Ok(s)
 }
