e6f9cd669a19680cc0ee4df8b9f33c9d9f513550
David Wickes <dave.wickes@gmail.com>
2017-03-06 08:53:15 +0000

    make your own trait - summarizable

---
+++ ch10/summarizable/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub author: String,
+    pub location: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
