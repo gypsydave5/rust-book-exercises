2934ba3ab7763a61a801ea6a80a98e51618233eb
David Wickes <dave.wickes@gmail.com>
2017-03-06 08:52:47 +0000

    trait bound

--- ch10/traits/src/main.rs
+++ ch10/traits/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     let nums = vec![5, 3, 7, 1, 10, 4];
-    let result = largest(&nums);
+    let result = largest_i32(&nums);
+    let result_generic = largest(&nums);
     println!("The largest i32 is {}.", result);
+    println!("The largest i32 by generic is {}.", result_generic);
 
     let chars = vec!['e', 'a', 's', 'y'];
-    let result = largest(&chars);
+    let result = largest_char(&chars);
+    let result_generic = largest(&chars);
     println!("The largest char is {}.", result);
+    println!("The largest char by generic is {}.", result_generic);
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -30,7 +34,7 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T:PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &i in list.iter() {
         if i > largest {
