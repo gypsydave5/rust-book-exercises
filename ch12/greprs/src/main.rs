ffac61bd37faec52cde9f8b45d3da341e0f68470
David Wickes <dave.wickes@gmail.com>
2017-03-12 18:00:36 +0000

    refactor - error handling with a Result

--- ch12/greprs/src/main.rs
+++ ch12/greprs/src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+                                                       println!("Problem parsing arguments: {}",
+                                                                err);
+                                                       process::exit(1);
+                                                   });
 
     println!("Searching for {}", config.search);
     println!("In file {}", config.filename);
@@ -23,13 +28,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let search = args[1].clone();
         let filename = args[2].clone();
 
-        Config {
-            search: search,
-            filename: filename,
-        }
+        Ok(Config {
+               search: search,
+               filename: filename,
+           })
     }
 }
