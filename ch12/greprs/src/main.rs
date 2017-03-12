0b5c9908880440d2062deed2606a2b310ed43c24
David Wickes <dave.wickes@gmail.com>
2017-03-12 21:26:19 +0000

    refactor: extract run and config into `lib.rs`

--- ch12/greprs/src/main.rs
+++ ch12/greprs/src/main.rs
@@ -1,8 +1,9 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,40 +16,8 @@ fn main() {
     println!("Searching for {}", config.search);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = greprs::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    search: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let search = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-               search: search,
-               filename: filename,
-           })
     }
 }
