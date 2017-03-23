880bcc2c9f7226add1fd78d61831f3d543c26467
David Wickes <dave.wickes@gmail.com>
2017-03-23 19:43:26 +0000

    getting a bit lispy

--- ch15/boxy/src/main.rs
+++ ch15/boxy/src/main.rs
@@ -1,3 +1,5 @@
+extern crate boxy;
+
 fn main() {
     boring_box();
 }
@@ -7,31 +9,3 @@ fn boring_box() {
     println!("b = {}", b);
 }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-use List::{Cons, Nil};
-
-fn listy() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-}
-
-fn cons(x: i32, l: List) -> List {
-    Cons(x, Box::new(l))
-}
-
-fn car(l: List) -> Option<i32> {
-    match l {
-        Cons(x, _) => Some(x),
-        Nil => None
-    }
-}
-
-fn cdr(l: List) -> List {
-    match l {
-        Cons(_, cdr) => *cdr,
-        Nil => Nil,
-    }
-}
