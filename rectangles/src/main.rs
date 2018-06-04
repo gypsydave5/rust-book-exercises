52f2f688c9c44dfac1140f38223cd7c92fe444cc
David Wickes <dave.wickes@gmail.com>
2018-06-04 09:45:49 +0100

    Rectangle - struct and #[derive(Debug)]

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect = (30, 50);
+    let rect = Rectangle {
+        width: 30,
+        height: 30,
+    };
 
     println!(
         "The area of the rectangle in {} square pixeles.",
-        area(rect)
+        area(&rect)
     );
+
+    println!("rect1 is {:?}", rect);
+    println!("rect1 is {:#?}", rect);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
