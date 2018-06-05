8fa8e7af47a6aad07c091f5933dd6d3f3a2494d5
David Wickes <dave.wickes@gmail.com>
2018-06-05 22:04:46 +0100

    enums and where

---
+++ counting_machine/src/main.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Ohio,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("seven"),
+        _ => (),
+    }
+
+    // `if let` - one match arm
+
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
