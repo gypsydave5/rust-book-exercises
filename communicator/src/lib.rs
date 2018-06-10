4cc3836236aa2019c5eb28c8ce1fe7978d8c41a8
David Wickes <dave.wickes@gmail.com>
2018-06-10 21:01:10 +0100

    module structure with communicator

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,7 +1,17 @@
+pub mod client;
+pub mod network;
+
 #[cfg(test)]
 mod tests {
+    use super::client;
+    use super::client::*;
+    use super::client::connect;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        ::client::connect(); // to the root!
+        super::client::connect(); // one up!
+        client::connect();
+        connect();
     }
 }
